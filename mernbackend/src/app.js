ecb78c88c5ba7ab1ca282e6ff3fc6dad996d9a10
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-01-08 22:40:29 +0530

    update: created form for registration

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -10,6 +10,7 @@ const static_path = path.join(__dirname, "../public/css");
 const templates_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
+
 app.use(express.static(static_path));
 app.set("view Engine", "hbs");
 
@@ -17,8 +18,15 @@ app.set("views" ,templates_path);
 hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-    res.render("index.hbs");
+    res.render("register.hbs");
 });
+
+app.get("./login.hbs",(req,res)=>{
+    res.render("login.hbs");
+});
+// app.get("/index",(req,res)=>{
+//     res.render("index.html");
+// });
 
 app.listen(port, () => {
     console.log(`server is running at port no ${port}`);
