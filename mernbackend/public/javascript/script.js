98a45de5841c15eb8506d470e8ce9589875db64d
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-01-14 14:44:00 +0530

    Update script.js

--- mernbackend/public/javascript/script.js
+++ mernbackend/public/javascript/script.js
@@ -23,74 +23,24 @@ let products = [
     name: "Operating Systems",
     tag: "book",
     price: 450,
-    inCart: 0,
+    inCart: 0
   },
   {
     name: "System Programming",
     tag: "book",
     price: 400,
-    inCart: 0,
+    inCart: 0
   },
   {
     name: "Compiler Design",
     tag: "book",
     price: 400,
-    inCart: 0,
+    inCart: 0
   },
   {
     name: "Computer science",
     tag: "book",
     price: 550,
-    inCart: 0,
-  },
+    inCart: 0
+  }
 ];
-
-for (let i = 0; i < carts.length; i++) {
-  // console.log("My loop");
-  carts[i].addEventListener("click", () => {
-    cartNumbers(products[i]);
-  });
-}
-
-function onLoadCartNumbers() {
-  let productNumbers = localStorage.getItem("cartNumbers");
-
-  if (productNumbers) {
-    document.querySelector(".carts span").textContent = productNumbers;
-  }
-}
-
-function cartNumbers(product) {
-  //   console.log("The product CLicked: ", product);
-  let productNumbers = localStorage.getItem("cartNumbers");
-  productNumbers = parseInt(productNumbers);
-
-  if (productNumbers) {
-    localStorage.setItem("cartNumbers", productNumbers + 1);
-    document.querySelector(".carts span").textContent = productNumbers + 1;
-  } else {
-    localStorage.setItem("cartNumbers", 1);
-    document.querySelector(".carts span").textContent = 1;
-  }
-  setItems(product);
-}
-
-function setItems(product) {
-  let cartItems = localStorage.getItem('productsInCart');
-  cartItems = JSON.parse(cartItems);
-  // console.log("My cart Items are",cartItems);
-
-if(cartItems != null){
-  cartItems[product.key].inCart += 1;
-}else{
-  product.inCart = 1;
-  cartItems = {
-    [product.tag]: product
-  }
-}
-
-
-  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-}
-
-onLoadCartNumbers();
