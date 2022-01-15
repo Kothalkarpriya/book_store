1999ea6422892ea0da2e7b9c1f8c00dfc77b8224
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-01-15 09:43:56 +0530

    update

--- mernbackend/public/javascript/script.js
+++ mernbackend/public/javascript/script.js
@@ -43,4 +43,62 @@ let products = [
     price: 550,
     inCart: 0
   }
-];
+]
+
+for(let i = 0 ; i< carts.length ; i++){
+  carts[i].addEventListener("click",()=>{
+    cartNumbers(products[i]);
+  })
+}
+
+function cartNumbers(product){
+  console.log("Products clicked ", product);
+  let productNumbers = localStorage.getItem('cartNumbers');
+  // console.log(typeof productNumbers);
+
+  productNumbers = parseInt(productNumbers);
+  // console.log(typeof productNumbers);
+
+  if(productNumbers){
+    localStorage.setItem('cartNumbers',productNumbers+1);
+    document.querySelector('.carts span').textContent = productNumbers +  1;
+  }else{
+    localStorage.setItem('cartNumbers',1);
+    document.querySelector('.carts span').textContent = 1;
+  }
+  setItems(product);
+}
+
+function setItems(product){
+  console.log("inside of SetItems");
+  console.log("My product is ",product);
+  let cartItems = localStorage.getItem('productsInCart');
+
+  cartItems = JSON.parse(cartItems);
+  // console.log("My cart ITems care", cartItems);
+
+  if(cartItems != null){
+    cartItems[product.tag].inCart += 1;  
+  }  
+  // }else if(){}
+  else{
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]:product
+    }
+  }
+  
+  localStorage.setItem("productsInCart",JSON.stringify(cartItems));
+}
+
+function onLoadCartNumbers(){
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  if(productNumbers){
+    document
+    .querySelector('.carts span').textContent = productNumbers;
+  }
+}
+
+
+onLoadCartNumbers();
